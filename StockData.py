2e92c4c62d5d717cfe0a57189ed7fbffee0be835
Helene <heldyrmose@deloitte.dk>
2021-12-09 13:33:38 +0100

    request IBM stock

--- StockData.py
+++ StockData.py
@@ -1,2 +1,16 @@
 import requests
 import pandas as pd
+
+# Requesting one stock from IBM, to see how it works :)
+response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=KKM3IZ0WUPZTREAK")
+
+
+# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+if response.status_code != 200:
+    raise ValueError("Could not retrieve data, code:", response.status_code)
+
+# The service sends JSON data, we parse that into a Python datastructure
+raw_data = response.json()
+
+print(raw_data)
