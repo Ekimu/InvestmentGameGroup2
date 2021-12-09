d5a851e9cbf135f7ed40811ced404d7dd5fa1327
Helene <heldyrmose@deloitte.dk>
2021-12-09 14:30:34 +0100

    extend input promt and add list of common stocks

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from StockData import requesting_one_stock
-
+print("Here is a list of some common stocks:")
+print("Tesla (TSLA), Apple (AAPL), Amazon (AMZN), Microsoft (MSFT), Nio Limited (NIO), Nvidia (NVDA), Moderna (MRNA), Nikola (NKLA), IBM (IBM).")
+print("You can choose any stock you want.")
 stock=input("Please select a stock: ")
 requesting_one_stock(stock)
