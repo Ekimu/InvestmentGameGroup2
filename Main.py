a576c60225053ccb408fc8da146b4eb8e312e4cc
ekimunukka <eki.munukka@deloitte.fi>
2021-12-09 16:44:32 +0200

    welcome

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 from StockData import requesting_one_stock
+print("Welcome to our Investment Game!!")
+cash_balance = 10
+print(f"Your starting balance is {cash_balance} EUR")
 print("Here is a list of some common stocks:")
 print("Tesla (TSLA), Apple (AAPL), Amazon (AMZN), Microsoft (MSFT), Nio Limited (NIO), Nvidia (NVDA), Moderna (MRNA), Nikola (NKLA), IBM (IBM).")
 print("You can choose any stock you want.")
