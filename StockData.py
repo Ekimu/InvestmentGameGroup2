8d27cbe5ee93f57762b002c6846b2247f75b65e2
Helene <heldyrmose@deloitte.dk>
2021-12-09 14:03:31 +0100

    change StockData.py to function

--- StockData.py
+++ StockData.py
@@ -1,19 +1,22 @@
 import requests
 import pandas as pd
 
-# Requesting one stock from IBM, to see how it works :)
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=KKM3IZ0WUPZTREAK")
+def requesting_one_stock(stock):
+    # Requesting one stock from IBM, to see how it works :)
+    response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock}&interval=1min&apikey=KKM3IZ0WUPZTREAK")
 
+    # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+    # See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+    if response.status_code != 200:
+        raise ValueError("Could not retrieve data, code:", response.status_code)
 
-# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-if response.status_code != 200:
-    raise ValueError("Could not retrieve data, code:", response.status_code)
+    # The service sends JSON data, we parse that into a Python datastructure
+    raw_data = response.json()
 
-# The service sends JSON data, we parse that into a Python datastructure
-raw_data = response.json()
+    data = raw_data['Time Series (1min)']
+    df = pd.DataFrame(data).T.apply(pd.to_numeric)
+    df.info()
+    print(df.head())
 
-data = raw_data['Time Series (1min)']
-df = pd.DataFrame(data).T.apply(pd.to_numeric)
-df.info()
-print(df.head())
+stock="IBM"
+requesting_one_stock(stock)
