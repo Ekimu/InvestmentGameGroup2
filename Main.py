ba4763b7c64a274195666a2b1609734ca384f706
Helene <heldyrmose@deloitte.dk>
2021-12-13 10:23:50 +0100

    Move to main program

--- Main.py
+++ Main.py
@@ -1,10 +1,3 @@
-from StockData import requesting_one_stock
-from graphs import creating_graph
-
-# Startup message / Log in
-username_input = input("Please enter your username: ")
-password_input = input("Please enter your password: ")
-print(f"Welcome to our Investment Game {username_input}!")
 
 
 # 3.a Search stock (based on stock symbol, names, market, currency, etc.)
@@ -15,31 +8,15 @@ while True:
         print("You decided not inspect any stock, we'll end here")
         break
     elif inspect == "y":
-        print("Here is a list of some common stocks:")
-        print("Tesla (TSLA), Apple (AAPL), Amazon (AMZN), Microsoft (MSFT), Nio Limited (NIO), Nvidia (NVDA), Moderna (MRNA), Nikola (NKLA), IBM (IBM).")
-        while True:
-            stock = input("Please select a stock (symbol): ")
-            datastock = requesting_one_stock(stock)
-            if datastock is None:
-                continue
 
-            creating_graph(datastock, stock)
-            currentprice = datastock["close"][0]
+
+
             buysell = input(f"Do you want to buy or sell {stock} for the current price {currentprice}? (b/s/n)")
 
 
         # 4. Buy stocks
             if buysell == "b":
-                amount = int(input(f"How many shares do you want to buy of {stock}?"))
-                costs = amount * currentprice
-                print(f"The total cost of the shares is: {costs}")
-                cash_balance = cash_balance - costs
-                if stock in Portfolio:
-                    Portfolio[stock] += amount
-                else:
-                    Portfolio[stock] = amount
-                print(f"Your current portfolio consists of the following stock: {Portfolio}")
-                print(f"Your current balance is now: {cash_balance}")
+
 
 
         # 5. Sell stocks
